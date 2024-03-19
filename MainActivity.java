0c135dc9bef48ba9f5f689541ede8e1a26321d1e
Ehsans <163805705+ehsansdev@users.noreply.github.com>
2024-03-19 11:15:00 +0800

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,42 @@
+package com.hmkcode;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hmkcode.adapters.MyFragmentPagerAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    MyFragmentPagerAdapter myFragmentPagerAdapter;
+    ViewPager viewPager;
+    TabLayout tabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+
+        setPagerAdapter();
+        setTabLayout();
+
+    }
+
+    private void setPagerAdapter(){
+        myFragmentPagerAdapter = new MyFragmentPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(myFragmentPagerAdapter);
+    }
+
+    private void setTabLayout() {
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(0).setText("First");
+        tabLayout.getTabAt(1).setText("Second");
+    }
+}
